0a5838fd35f13804f33cee0fbd84251002516cd0
Robert <robert.schleicher@drake.edu>
2026-03-12 12:47:38 -0500

    Exercise 1

--- flaskapp.py
+++ flaskapp.py
@@ -28,7 +28,10 @@ def hello(name):
 #  YOUR ROUTES GO BELOW THIS LINE
 #  Each exercise asks you to add a new @app.route here
 # ============================================================
-
+@app.route('/analyze/<word>')
+def analyze(word):
+    count = len(word)
+    return str(count)
 
 
 
